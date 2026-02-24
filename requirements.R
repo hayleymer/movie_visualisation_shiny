d220ed180b224a6fa90fe14564f4e17f32849f5a
hayleymer <hayleymerat@gmail.com>
2026-02-25 10:14:33 +1100

    Add R package requirements for the project

---
+++ requirements.R
@@ -0,0 +1,13 @@
+## Requirements
+
+library(shiny)
+library(shinythemes)
+library(shinydashboard)
+library(ggplot2)
+library(plotly)
+library(readxl)
+library(ggvis)
+library(dplyr)
+library(lubridate)
+library(htmltools)
+library(dtplyr)
